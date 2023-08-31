1ee636ed24426607acafb9e76101d35632e7d8e6
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-08-31 17:04:47 -0300

    ...

---
+++ javascript.js
@@ -0,0 +1,26 @@
+const card = document.querySelector('#card');
+const botao1 = document.getElementById('b1');
+const botao2 = document.getElementById('b2');
+const botao3 = document.getElementById('b3');
+const botao4 = document.getElementById('b4');
+const front = document.getElementById('front');
+const back = document.getElementById('back');
+var count = 0;
+var texts = ['São', 'Paulo', 'Futebol', 'Clube'];
+
+function flip() {
+    card.classList.toggle('flip');
+    count++;
+}
+
+function textos(n) {
+    if (count % 2 == 0) {
+        //Trás da carta
+        back.innerHTML = `<h1>${texts[n]}</h1>`;
+        flip();
+    } else {
+        //Frente da carta
+        front.innerHTML = `<h1>${texts[n]}</h1>`;
+        flip();
+    }
+}
